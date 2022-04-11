477535486ff440999ce7448d0bf19c549d4e7fa6
Gentiana Idrizi <93874728+Gentiana-Idrizi@users.noreply.github.com>
2022-04-11 22:45:13 +0200

    Update client.cpp

--- ServerClientWindowsC++/client.cpp
+++ ServerClientWindowsC++/client.cpp
@@ -10,3 +10,25 @@ void main()
 string ipAddress = "127.0.0.1"; //IP address of the server
 int port = 54000; //Listening port of the server
 
+//Initialize Winsock
+WSADATA data;
+
+WORD ver = MAKEWORD(2, 2);
+int wsResult = WSAStartup(ver, &data);
+if (wsResult != 0)
+{
+cerr << "Can't start winsock, Err #" << wsResult << endl;
+return;
+}
+
+
+//Create SOCKET
+SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+if (sock == INVALID_SOCKET)
+{
+cerr << "Can't create socket, Err #" <<WSAGetLastError() << endl;
+WSACleanup;
+return;
+}
+
+
